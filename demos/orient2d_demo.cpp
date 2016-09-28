85151b7c45da804fdcf1459a929c17814d78b1b2
Taras Zakharko <tzakharko@dhcp-plk-spw-130-60-153-138.uzh.ch>
2016-09-28 13:19:55 +0200

    Added a comparison with simd_orient() + timings

--- demos/orient2d_demo.cpp
+++ demos/orient2d_demo.cpp
@@ -5,6 +5,12 @@
 #include <predicates.hpp>
 #include "write_png.h"
 
+#include<stdio.h>
+#include<simd/simd.h>
+
+#include <CoreServices/CoreServices.h>
+
+
 using namespace predicates;
 
 int main()
@@ -12,12 +18,37 @@ int main()
   const double x1[] = {0.5, 0.5};
   const double x2[] = {12.0, 12.0};
   const double x3[] = {24.0, 24.0};
+  
+  
+  // const double x1[] = {0.5, 0.5};
+  // const double x2[] = {-100, -100};
+  // const double x3[] = {100, 100};
+  
+  const vector_double2 _x2 = {x2[0], x2[1]};
+  const vector_double2 _x3 = {x3[0], x3[1]};
 
   const size_t nx = 256, ny = 256;
 
-  auto predicate1 = [&](const double * x) { return orient2dfast(x,  x2, x3); };
-  auto predicate2 = [&](const double * x) { return orient2dfast(x3, x,  x2); };
-  auto predicate3 = [&](const double * x) { return orient2dfast(x2, x3, x ); };
+  // auto predicate1 = [&](const double * x) { return orient2d(x,  x2, x3); };
+  // auto predicate2 = [&](const double * x) { return orient2dfast(x, x2,  x3); };
+  // auto predicate3 = [&](const double * x) {
+  //   const vector_double2 _x = {x[0], x[1]};
+  //
+  //   return simd_orient(_x2-_x, _x3-_x);
+  //   //return orient2d(x, x2, x3 );
+  // };
+
+  auto predicate1 = [&](const double * x) { return orient2d( x3, x, x2); };
+  auto predicate2 = [&](const double * x) { return orient2dfast(x3, x, x2); };
+  auto predicate3 = [&](const double * x) { 
+    const vector_double2 _x = {x[0], x[1]};
+    
+    return simd_orient(_x3 - _x2,  _x-_x2);
+    //return orient2d(x, x2, x3 ); 
+    
+
+  };
+
 
   double A[nx * ny];
 
@@ -27,14 +58,26 @@ int main()
       A[nx * i + j] = p;
     };
 
-  perturb2d(predicate1, x1, nx, ny, callback);
-  matrix_to_png("orientation1.png", nx, ny, A);
+    double t0 = CFAbsoluteTimeGetCurrent();
+    perturb2d(predicate1, x1, nx, ny, callback);
+  	double elapsed = CFAbsoluteTimeGetCurrent() - t0;
+  	printf("Elapsed time for predicate1=%fms\n", elapsed*1000);
+    
+    matrix_to_png("orientation1.png", nx, ny, A);
 
-  perturb2d(predicate2, x1, nx, ny, callback);
-  matrix_to_png("orientation2.png", nx, ny, A);
+  	t0 = CFAbsoluteTimeGetCurrent();
+    perturb2d(predicate2, x1, nx, ny, callback);
+  	elapsed = CFAbsoluteTimeGetCurrent() - t0;
+  	printf("Elapsed time for predicate2=%fms\n", elapsed*1000);
+	
+    matrix_to_png("orientation2.png", nx, ny, A);
 
-  perturb2d(predicate3, x1, nx, ny, callback);
-  matrix_to_png("orientation3.png", nx, ny, A);
+  	t0 = CFAbsoluteTimeGetCurrent();
+    perturb2d(predicate3, x1, nx, ny, callback);
+  	elapsed = CFAbsoluteTimeGetCurrent() - t0;
+  	printf("Elapsed time for predicate3=%fms\n", elapsed*1000);
+	
+    matrix_to_png("orientation3.png", nx, ny, A);
 
   return 0;
 }
